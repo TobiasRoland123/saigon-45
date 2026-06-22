e61d0ca14916cab67d4dd14ea9e30d92a239817f
Tobias <samueltobiasrolanduyet@gmail.com>
2026-06-22 23:34:28 +0200

    testing pre commit hook

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -1,7 +1,6 @@
 'use client'
 import { useHeaderTheme } from '@/providers/HeaderTheme'
 import React, { useEffect } from 'react'
-
 import type { Page } from '@/payload-types'
 import { CMSLink } from '@/components/Link'
 import { Media } from '@/components/Media'
