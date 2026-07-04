2b84c348a08c614f5206b00b968d8d1bfd3e2690
JBMadvig <jonas.e.madvig@gmail.com>
2026-07-04 22:48:33 +0200

    fix(Header): Header has headerTheme removed + container fix
    Removed all states associated with headerTheme and re-introduced the
    container class, but not on the `<header>` but in a wrapper div inside
    the header.

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,8 +1,6 @@
 'use client'
-import { useHeaderTheme } from '@/providers/HeaderTheme'
 import Link from 'next/link'
-import { usePathname } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React from 'react'
 
 import type { Header } from '@/payload-types'
 
@@ -15,20 +13,9 @@ interface HeaderClientProps {
 }
 
 export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
-  const { headerTheme, setHeaderTheme } = useHeaderTheme()
-  const pathname = usePathname()
-
-  useEffect(() => {
-    setHeaderTheme(null)
-  }, [pathname, setHeaderTheme])
-
   return (
-    <header
-      suppressHydrationWarning
-      className="px-6 relative z-20 bg-surface max-w-full mx-0"
-      {...(headerTheme ? { 'data-theme': headerTheme } : {})}
-    >
-      <div className="py-6 flex justify-between items-center">
+    <header suppressHydrationWarning className="relative z-20 bg-surface">
+      <div className="container px-6 py-6 flex justify-between items-center">
         <Link href="/" className="flex items-center flex-row gap-2">
           <p className="text-2xl md:text-3xl font-bold text-surface-tint">Saigon 45</p>
           <SvgMedia className="h-8 w-8 md:h-12 md:w-12" resource={data.media} />
