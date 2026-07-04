7c2b623682cb771696c2b8feba04ac829f32a83e
JBMadvig <jonas.e.madvig@gmail.com>
2026-07-04 22:53:06 +0200

    test(migration): DB migration

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20260701_122522 from './20260701_122522'
 import * as migration_20260701_000000_add_media_blur_placeholder from './20260701_000000_add_media_blur_placeholder'
 import * as migration_20260703_215908_feature_highlights_block from './20260703_215908_feature_highlights_block'
 import * as migration_20260703_191909 from './20260703_191909'
+import * as migration_20260704_205136 from './20260704_205136'
 
 export const migrations = [
   {
@@ -29,5 +30,10 @@ export const migrations = [
     up: migration_20260703_191909.up,
     down: migration_20260703_191909.down,
     name: '20260703_191909',
+  },
+  {
+    up: migration_20260704_205136.up,
+    down: migration_20260704_205136.down,
+    name: '20260704_205136',
   },
 ]
