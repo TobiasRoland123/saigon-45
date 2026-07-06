940e262aa130ab3a781c4735e2e0e0c88a028a66
JBMadvig <jonas.e.madvig@gmail.com>
2026-07-06 23:28:16 +0200

    feat(OpenHeroPill): Pill to show address and open status
    On the hero we now have the pill that shows the address, a link to
    google maps or what ever url they want and a system that shows if they
    are open or closed ++ what time the open if they are closed or if they
    are open until what time.

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -13,6 +13,7 @@ import { Posts } from './collections/Posts'
 import { Users } from './collections/Users'
 import { Footer } from './Footer/config'
 import { Header } from './Header/config'
+import { OpeningHours } from './OpeningHours/config'
 import { plugins } from './plugins'
 import { defaultLexical } from '@/fields/defaultLexical'
 import { getServerSideURL } from './utilities/getURL'
@@ -88,7 +89,7 @@ export default buildConfig({
       token: process.env.BLOB_READ_WRITE_TOKEN || '',
     }),
   ],
-  globals: [Header, Footer],
+  globals: [Header, Footer, OpeningHours],
   secret: process.env.PAYLOAD_SECRET,
   sharp,
   typescript: {
