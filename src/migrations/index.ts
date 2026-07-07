c5164c7a18536f184c2f0f22ac7c9513786f0896
Samuel Tobias Roland Uyet <samueltobiasrolanduyet@gmail.com>
2026-07-07 22:03:16 +0200

    Add migration for menu highlights block and related tables

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,7 +1,8 @@
 import * as migration_20260409_155721_initial from './20260409_155721_initial'
-import * as migration_20260701_122522 from './20260701_122522'
 import * as migration_20260701_000000_add_media_blur_placeholder from './20260701_000000_add_media_blur_placeholder'
+import * as migration_20260701_122522 from './20260701_122522'
 import * as migration_20260703_215908_feature_highlights_block from './20260703_215908_feature_highlights_block'
+import * as migration_20260707_200303 from './20260707_200303'
 
 export const migrations = [
   {
@@ -10,18 +11,23 @@ export const migrations = [
     name: '20260409_155721_initial',
   },
   {
-    up: migration_20260701_122522.up,
-    down: migration_20260701_122522.down,
-    name: '20260701_122522',
-  },
-  {
     up: migration_20260701_000000_add_media_blur_placeholder.up,
     down: migration_20260701_000000_add_media_blur_placeholder.down,
     name: '20260701_000000_add_media_blur_placeholder',
   },
   {
+    up: migration_20260701_122522.up,
+    down: migration_20260701_122522.down,
+    name: '20260701_122522',
+  },
+  {
     up: migration_20260703_215908_feature_highlights_block.up,
     down: migration_20260703_215908_feature_highlights_block.down,
     name: '20260703_215908_feature_highlights_block',
+  },
+  {
+    up: migration_20260707_200303.up,
+    down: migration_20260707_200303.down,
+    name: '20260707_200303',
   },
 ]
