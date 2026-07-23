14a7028033e7a4201b5b17bb977fbf4139a06cc0
Tobias <samueltobiasrolanduyet@gmail.com>
2026-07-23 09:45:06 +0200

    fixed migration issue

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -18,7 +18,7 @@ import * as migration_20260713_194909 from './20260713_194909'
 import * as migration_20260714_235000_restore_opening_hours from './20260714_235000_restore_opening_hours'
 import * as migration_20260716_191428_hero_badge from './20260716_191428_hero_badge'
 import * as migration_20260722_115040 from './20260722_115040'
-import * as migration_20260722_201500_menu_item_number from './20260722_201500_menu_item_number'
+import * as migration_20260723_072602_add_menu_item_number from './20260723_072602_add_menu_item_number'
 
 export const migrations = [
   {
@@ -122,8 +122,8 @@ export const migrations = [
     name: '20260722_115040',
   },
   {
-    up: migration_20260722_201500_menu_item_number.up,
-    down: migration_20260722_201500_menu_item_number.down,
-    name: '20260722_201500_menu_item_number',
+    up: migration_20260723_072602_add_menu_item_number.up,
+    down: migration_20260723_072602_add_menu_item_number.down,
+    name: '20260723_072602_add_menu_item_number',
   },
 ]
